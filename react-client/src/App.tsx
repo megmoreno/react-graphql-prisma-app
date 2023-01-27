e201d01982152c2aef391cc3ac7b834cea5d7381
MEGAN ARNELL <arnell.megan@yahoo.com>
2023-01-26 17:13:09 -0700

    Added NavBar

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,6 +1,7 @@
 import StudentDisplay from './components/StudentDisplay'
 import { useQuery } from 'urql'
 import { GetStudentsDocument } from './graphql/generated'
+import NavBar from './components/NavBar'
 
 function App() {
   const [results] = useQuery({
@@ -8,10 +9,13 @@ function App() {
   })
 
   return (
-    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      {results.data?.students.map((student, i) => (
-        <StudentDisplay student={student} key={i} />
-      ))}
+    <div>
+      <NavBar />
+      <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+        {results.data?.students.map((student, i) => (
+          <StudentDisplay student={student} key={i} />
+        ))}
+      </div>
     </div>
   )
 }
