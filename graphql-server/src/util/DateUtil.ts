d6089ec071e63b1d6be796ff368cee2ceb3747f9
MEGAN ARNELL <arnell.megan@yahoo.com>
2023-01-27 19:07:31 -0700

    getting saturday (last day) of week in util class + tests

--- graphql-server/src/util/DateUtil.ts
+++ graphql-server/src/util/DateUtil.ts
@@ -3,3 +3,10 @@ export function getSundayOfCurrentWeek(currentDate: Date) {
   const diff = currentDate.getDate() - dayOfWeek
   return new Date(currentDate.setDate(diff))
 }
+
+export function getSaturdayOfCurrentWeek(currentDate: Date) {
+  const dayOfWeek = currentDate.getDay()
+  const numDaysToAdd = 6 - dayOfWeek
+  const diff = currentDate.getDate() + numDaysToAdd
+  return new Date(currentDate.setDate(diff))
+}
--- graphql-server/test/util/DateUtil.test.ts
+++ graphql-server/test/util/DateUtil.test.ts
@@ -1,9 +1,66 @@
-import { getSundayOfCurrentWeek } from '../../src/util/DateUtil'
+import {
+  getSundayOfCurrentWeek,
+  getSaturdayOfCurrentWeek,
+} from '../../src/util/DateUtil'
 
 describe('testing getSundayOfCurrentWeek', () => {
   test('input Thursday, January 26, 2023 should return Sunday, January 22, 2023', () => {
     const sunday = new Date(2023, 0, 22, 0, 0, 0, 0)
     const thursday = new Date(2023, 0, 26, 0, 0, 0, 0)
     expect(getSundayOfCurrentWeek(thursday)).toMatchObject(sunday)
+  })
+})
+
+describe('testing getSundayOfCurrentWeek', () => {
+  test('input Saturday, January 28, 2023 should return Sunday, January 22, 2023', () => {
+    const sunday = new Date(2023, 0, 22, 0, 0, 0, 0)
+    const saturday = new Date(2023, 0, 28, 0, 0, 0, 0)
+    expect(getSundayOfCurrentWeek(saturday)).toMatchObject(sunday)
+  })
+})
+
+describe('testing getSundayOfCurrentWeek', () => {
+  test('input Sunday, January 22, 2023 should return Sunday, January 22, 2023', () => {
+    const sunday = new Date(2023, 0, 22, 0, 0, 0, 0)
+    expect(getSundayOfCurrentWeek(sunday)).toMatchObject(sunday)
+  })
+})
+
+describe('testing getSundayOfCurrentWeek', () => {
+  test('input Monday, January 23, 2023 should return Sunday, January 22, 2023', () => {
+    const sunday = new Date(2023, 0, 22, 0, 0, 0, 0)
+    const monday = new Date(2023, 0, 23, 0, 0, 0, 0)
+    expect(getSundayOfCurrentWeek(monday)).toMatchObject(sunday)
+  })
+})
+
+describe('testing getSaturdayOfCurrentWeek', () => {
+  test('input Thursday, January 26, 2023 should return Saturday, January 28, 2023', () => {
+    const saturday = new Date(2023, 0, 28, 0, 0, 0, 0)
+    const thursday = new Date(2023, 0, 26, 0, 0, 0, 0)
+    expect(getSaturdayOfCurrentWeek(thursday)).toMatchObject(saturday)
+  })
+})
+
+describe('testing getSaturdayOfCurrentWeek', () => {
+  test('input Sunday, January 22, 2023 should return Saturday, January 28, 2023', () => {
+    const saturday = new Date(2023, 0, 28, 0, 0, 0, 0)
+    const sunday = new Date(2023, 0, 22, 0, 0, 0, 0)
+    expect(getSaturdayOfCurrentWeek(sunday)).toMatchObject(saturday)
+  })
+})
+
+describe('testing getSaturdayOfCurrentWeek', () => {
+  test('input Friday, January 27, 2023 should return Saturday, January 28, 2023', () => {
+    const saturday = new Date(2023, 0, 28, 0, 0, 0, 0)
+    const friday = new Date(2023, 0, 23, 0, 0, 0, 0)
+    expect(getSaturdayOfCurrentWeek(friday)).toMatchObject(saturday)
+  })
+})
+
+describe('testing getSaturdayOfCurrentWeek', () => {
+  test('input Saturday, January 28, 2023 should return Saturday, January 28, 2023', () => {
+    const saturday = new Date(2023, 0, 28, 0, 0, 0, 0)
+    expect(getSaturdayOfCurrentWeek(saturday)).toMatchObject(saturday)
   })
 })
