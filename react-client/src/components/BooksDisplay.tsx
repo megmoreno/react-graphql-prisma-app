3be16dc3c14b7ffb945c36b2fd57f36713791781
MEGAN ARNELL <arnell.megan@yahoo.com>
2023-07-31 15:47:04 -0600

    Updated packages and add key to list to fix bug

--- react-client/src/components/BooksDisplay.tsx
+++ react-client/src/components/BooksDisplay.tsx
@@ -10,8 +10,8 @@ function BooksDisplay({ books }: Props) {
       <h1 className="text-med text-zinc-700 font-bold px-4">
         Books we're reading
       </h1>
-      {books?.map((book) => (
-        <li>{book.title}</li>
+      {books?.map((book, i) => (
+        <li key={i}>{book.title}</li>
       ))}
     </div>
   )
